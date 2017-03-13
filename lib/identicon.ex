d5e13630c1bc7c2f783ca9ca7e8d758bd34c2d6f
Laurent Curau <laurentcurau@gmail.com>
2017-03-13 14:27:12 +0000

    #pick_color + fix tests

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,15 +6,28 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+  end
+
+  @doc """
+  Picks a color for the identicon.
+
+  ## Examples
+      iex> image = %Identicon.Image{hex: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]}
+      iex> Identicon.pick_color(image)
+      %Identicon.Image{color: {114, 179, 2}, hex: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]}
+  """
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   @doc """
   Converts a string into a unique sequence of characters.
 
   ## Examples
-      iex> hash = :crypto.hash(:md5, "banana")
-      iex> :binary.bin_to_list(hash)
-      [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
+      iex> Identicon.hash_input("banana")
+      %Identicon.Image{hex: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]}
   """
 
   def hash_input(input) do
